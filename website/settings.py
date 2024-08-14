fa7398baedb3d571e87b9cc58e256773f6cce870
ArunBhalla26 <166276436+ArunBhalla26@users.noreply.github.com>
2024-08-15 00:01:33 +0530

    sucessfully added and backed up data to postgress sql

--- website/settings.py
+++ website/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'giftos database',
+        'USER': 'giftos admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '2000',
     }
 }
+
 
 
 # Password validation
